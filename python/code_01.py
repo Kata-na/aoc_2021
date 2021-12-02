457b8730b2409a8d36c154a9bedf58737b9ef07e
Katazina Zacharevic <zacharevic.katazina@gmail.com>
2021-12-03 01:05:15 +0200

    optimized part 2 (python)

--- python/code_01.py
+++ python/code_01.py
@@ -3,11 +3,12 @@ import numpy as np
 
 path = './data/dt_01.txt'
 m = fread(path).to_list()[0]
+m = np.array(m)
 
 ## PART 1
 ans = np.sum(np.diff(m) > 0)
 print(f"Answer: {ans}")
 
 ## PART 2
-ans = np.sum(m > np.roll(m, shift=3))
+ans = np.sum(m[3:] > m[:len(m)-3])
 print(f"Answer: {ans}")
